7e77c33e45f6b08038f3274b04cf54f03783f220
javiermesa85 <javiermesa85@dbb8e0fe-aea2-11de-b826-45995d83e13f>
2009-11-26 02:34:38 +0000

    Adding first registration page

---
+++ trunk/PWAS_Site/PWAS_Site/Register.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PWAS_Site
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void ddState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
