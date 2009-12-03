c263ed31ebae8b09a88279fee5aba1a2293c8d76
dulcardo <dulcardo@dbb8e0fe-aea2-11de-b826-45995d83e13f>
2009-12-03 04:56:11 +0000

    adding view order

---
+++ trunk/PWAS_Site/PWAS_Site/customer_view_order.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/PWAS.Master" AutoEventWireup="true" CodeBehind="customer_view_order.aspx.cs" Inherits="PWAS_Site.Formulario_web11" Title="Página sin título" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="page_title" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="box1_text" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="box1_link" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="box2_text" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="box2_link" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="box3_text" runat="server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="box3_link" runat="server">
+</asp:Content>
+<asp:Content ID="Content8" ContentPlaceHolderID="navigation_menu" runat="server">
+</asp:Content>
+<asp:Content ID="Content9" ContentPlaceHolderID="body_title" runat="server">
+</asp:Content>
+<asp:Content ID="Content10" ContentPlaceHolderID="body_content" runat="server">
+<asp:Label ID="lblNotify" Visible="false" runat="server" ></asp:Label>
+<asp:Table ID="orderHistoryTable" runat="server">
+    <asp:TableRow ID="headerTable1" runat="server" CssClass="orderTopRow">
+        <asp:TableHeaderCell ID="TableHeaderCell1" runat="server" Text="Job  #" Width="20%" ></asp:TableHeaderCell>
+        <asp:TableHeaderCell ID="TableHeaderCell2" runat="server" Text="Job Name" Width="35%" ></asp:TableHeaderCell>
+        <asp:TableHeaderCell ID="TableHeaderCell3" runat="server" Text="Status" Width="35%" ></asp:TableHeaderCell>
+        <asp:TableHeaderCell ID="TableHeaderCell4" runat="server" Text="Date" Width="40%"  ></asp:TableHeaderCell>
+    </asp:TableRow>
+</asp:Table>
+</asp:Content>
---
+++ trunk/PWAS_Site/PWAS_Site/customer_view_order.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+
+
+using PWAS.Model;
+using PWAS.DataAccess.Interfaces;
+
+
+namespace PWAS_Site
+{
+    public partial class Formulario_web11 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
+            
+            int userID = (int)Session[Constants.PWAS_SESSION_ID];
+            
+
+            var query = from p in orderRepository.Orders where p.userID == userID select p;
+
+
+            foreach (var ord in query)
+            {
+                TableRow row = new TableRow();
+
+                TableCell cellOrderID = new TableCell();
+                TableCell cellJobName = new TableCell();
+                TableCell cellStatus = new TableCell();
+                TableCell cellDate = new TableCell();
+
+                cellOrderID.Controls.Add(new LiteralControl(ord.orderID.ToString()));
+                cellJobName.Controls.Add(new LiteralControl(ord.job_name.ToString()));
+                cellStatus.Controls.Add(getStatusControl(ord.orderID));
+
+                cellDate.Controls.Add(new LiteralControl(getCurrentOrderStatusDate(ord.orderID)));
+
+                row.Cells.Add(cellOrderID);
+                row.Cells.Add(cellJobName);
+                row.Cells.Add(cellStatus);
+                row.Cells.Add(cellDate);
+
+                row.CssClass = "orderRow";
+
+                this.orderHistoryTable.Rows.Add(row);
+            }
+        }
+
+        private Control getStatusControl(int orderID)
+        {
+            Status status = getCurrentOrderStatus(orderID);
+
+            Control ret = new LiteralControl("---");
+            LinkButton payButton;
+
+            if (status.statusId == 1)
+            {
+                payButton = new LinkButton();
+                payButton.ID = orderID + "";
+                payButton.Text = "Pay Now";
+                payButton.Visible = true;
+                payButton.Command += new CommandEventHandler(func_Pay);
+
+                ret = payButton;
+            }
+            else
+            {
+                ret = new LiteralControl(status.statusName);
+            }
+
+            return ret;
+        }
+        
+
+        private void func_Pay(object sender, CommandEventArgs e)
+        {
+            this.lblNotify.Text = "Order Sucessfully Paid!";
+            this.lblNotify.ForeColor = System.Drawing.Color.Blue;
+            this.lblNotify.Visible = true;
+            updateOrderStatus(System.Int32.Parse(((LinkButton)sender).ID.ToString()));
+            
+        }
+
+        private void updateOrderStatus(int orderID)
+        {
+            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
+            IStatusRepository statusReposiory = RepositoryFactory.Get<IStatusRepository>();
+            Order currentOrder = orderRepository.GetById(orderID);                       
+
+            Order orderCreate = new Order
+            {
+                orderID = currentOrder.orderID,
+                userID = currentOrder.userID,
+                job_name = currentOrder.job_name,
+                width = currentOrder.width,
+                height = currentOrder.height,
+                quantity = currentOrder.quantity,
+                stock_finish = currentOrder.stock_finish,
+                stock_weight = currentOrder.stock_weight,
+                two_sided = currentOrder.two_sided,
+                folded = currentOrder.folded,
+                ship =  currentOrder.ship,
+                Status = statusReposiory.GetById(2)
+            };
+                
+            orderRepository.UpdateOrderInfo(orderCreate);
+        }
+
+        private Status getCurrentOrderStatus(int orderID)
+        {          
+            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
+            IStatusRepository statusReposiory = RepositoryFactory.Get<IStatusRepository>();
+            Order currentOrder = orderRepository.GetById(orderID);
+            Status currentStatus = statusReposiory.GetById(currentOrder.Status.statusId);
+
+            return currentStatus;
+        }
+
+        private string getCurrentOrderStatusDate(int orderID)
+        {
+            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
+            IOrderHistoryRepository historyRepository = RepositoryFactory.Get<IOrderHistoryRepository>();
+
+            Order currentOrder = orderRepository.GetById(orderID);
+
+            OrderHistory currentHistory = historyRepository.OrderHistories.Single(x => x.orderId == orderID && x.statusId == currentOrder.Status.statusId);// & x.statusId = currentOrder.Status);
+
+            return currentHistory.modifiedDate.ToString();            
+        }
+
+    }
+}
