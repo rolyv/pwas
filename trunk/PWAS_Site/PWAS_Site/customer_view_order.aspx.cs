47a297ac2e2f53a665959214b2ebc42f80b6e7b9
javiermesa85 <javiermesa85@dbb8e0fe-aea2-11de-b826-45995d83e13f>
2009-12-05 20:36:57 +0000

    Committing changes on behalf of Dulcardo

--- trunk/PWAS_Site/PWAS_Site/customer_view_order.aspx.cs
+++ trunk/PWAS_Site/PWAS_Site/customer_view_order.aspx.cs
@@ -18,36 +18,60 @@ using PWAS.DataAccess.Interfaces;
 
 namespace PWAS_Site
 {
-    public partial class Formulario_web11 : System.Web.UI.Page
+    public partial class customer_view_order : System.Web.UI.Page
     {
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.tblViewOrder.Visible = true;
+            this.formEditOrder.Visible = false;
+
             IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
-            
+
             int userID = (int)Session[Constants.PWAS_SESSION_ID];
-            
-
-            var query = from p in orderRepository.Orders where p.userID == userID select p;
 
 
-            foreach (var ord in query)
+            var query = from p
+                        in orderRepository.Orders
+                        where p.userID == userID
+                        select p;
+
+            foreach (var current_order in query)
             {
                 TableRow row = new TableRow();
 
                 TableCell cellOrderID = new TableCell();
                 TableCell cellJobName = new TableCell();
+                TableCell cellPrice = new TableCell();
                 TableCell cellStatus = new TableCell();
                 TableCell cellDate = new TableCell();
+                TableCell cellEdit = new TableCell();
+                TableCell cellView = new TableCell();
 
-                cellOrderID.Controls.Add(new LiteralControl(ord.orderID.ToString()));
-                cellJobName.Controls.Add(new LiteralControl(ord.job_name.ToString()));
-                cellStatus.Controls.Add(getStatusControl(ord.orderID));
 
-                cellDate.Controls.Add(new LiteralControl(getCurrentOrderStatusDate(ord.orderID)));
+                cellOrderID.Controls.Add(new LiteralControl(current_order.orderID.ToString()));
+                cellJobName.Controls.Add(new LiteralControl(current_order.job_name.ToString()));
+                cellPrice.Controls.Add(new LiteralControl(getOrderPrice(current_order.orderID)));
+                cellStatus.Controls.Add(getStatusControl(current_order));
+                cellDate.Controls.Add(new LiteralControl(OrderUtilities.getCurrentOrderStatusDate(current_order.orderID)));
+                cellEdit.Controls.Add(getEditButton(current_order));
+                cellView.Controls.Add(getViewButton(current_order));
 
+                //pixels
+                cellEdit.Width = Unit.Pixel(50);
+                cellView.Width = Unit.Pixel(50);
+                cellOrderID.Width = Unit.Pixel(100);
+                cellJobName.Width = Unit.Pixel(200);
+                cellPrice.Width = Unit.Pixel(150);
+                cellStatus.Width = Unit.Pixel(150);
+                cellDate.Width = Unit.Pixel(200);
+
+
+                row.Cells.Add(cellEdit);
+                row.Cells.Add(cellView);
                 row.Cells.Add(cellOrderID);
                 row.Cells.Add(cellJobName);
+                row.Cells.Add(cellPrice);
                 row.Cells.Add(cellStatus);
                 row.Cells.Add(cellDate);
 
@@ -57,19 +81,50 @@ namespace PWAS_Site
             }
         }
 
-        private Control getStatusControl(int orderID)
+        private Control getViewButton(Order current_order)
         {
-            Status status = getCurrentOrderStatus(orderID);
+            ImageButton viewButton = new ImageButton();
+
+            viewButton.ID = current_order.orderID + "v";
+            viewButton.CommandArgument = current_order.orderID.ToString();
+            viewButton.ImageUrl = "/images/left-list.gif";
+            viewButton.Visible = true;
+            viewButton.Command += new CommandEventHandler(func_View);
+
+            return viewButton;
+        }
+
+        private Control getEditButton(Order current_order)
+        {
+
+            ImageButton editButton = new ImageButton();
+
+            editButton.ID = current_order.orderID + "e";
+            editButton.CommandArgument = current_order.orderID.ToString();
+            editButton.ImageUrl = "/images/edit.gif";
+            editButton.Visible = true;
+            editButton.Command += new CommandEventHandler(func_Edit);
+
+
+
+            return editButton;
+        }
+
+        private Control getStatusControl(Order currentOrder)
+        {
+            Status status = currentOrder.Status;
 
             Control ret = new LiteralControl("---");
-            LinkButton payButton;
+            Button payButton;
 
             if (status.statusId == 1)
             {
-                payButton = new LinkButton();
-                payButton.ID = orderID + "";
+                payButton = new Button();
+                payButton.ID = currentOrder.orderID + "s";
                 payButton.Text = "Pay Now";
                 payButton.Visible = true;
+                payButton.Width = Unit.Pixel(150);
+                payButton.CommandArgument = currentOrder.orderID.ToString();
                 payButton.Command += new CommandEventHandler(func_Pay);
 
                 ret = payButton;
@@ -81,49 +136,144 @@ namespace PWAS_Site
 
             return ret;
         }
-        
 
         private void func_Pay(object sender, CommandEventArgs e)
         {
             this.lblNotify.Text = "Order Sucessfully Paid!";
             this.lblNotify.ForeColor = System.Drawing.Color.Blue;
             this.lblNotify.Visible = true;
-            updateOrderStatus(System.Int32.Parse(((LinkButton)sender).ID.ToString()));
-            
-        }
 
-        private void updateOrderStatus(int orderID)
-        {
-            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
-            IStatusRepository statusReposiory = RepositoryFactory.Get<IStatusRepository>();
-            Order currentOrder = orderRepository.GetById(orderID);
-            currentOrder.Status = new Status { statusId= 2,statusName = "Paid"};// statusReposiory.GetById(2);
+            int orderID = System.Int32.Parse(e.CommandArgument.ToString());
+            OrderUtilities.updateOrderStatus(orderID, 2);
 
-            orderRepository.SubmitChanges();
-            
             Response.Redirect(Request.Url.ToString());
         }
 
-        private Status getCurrentOrderStatus(int orderID)
-        {          
-            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
-            IStatusRepository statusReposiory = RepositoryFactory.Get<IStatusRepository>();
-            Order currentOrder = orderRepository.GetById(orderID);
-            Status currentStatus = statusReposiory.GetById(currentOrder.Status.statusId);
+        private void func_Edit(object sender, CommandEventArgs e)
+        {
+            this.formEditOrder.Visible = true;
+            this.tblViewOrder.Visible = false;
 
-            return currentStatus;
+            IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
+
+            int orderID = System.Int32.Parse(e.CommandArgument.ToString());
+            Session["PWAS_order_id"] = orderID;
+
+            Order current_order = orderRepository.GetById(orderID);
+
+            this.txtJobName.Text = current_order.job_name;
+            this.txtFinalSizeX.Text = current_order.width.ToString();
+            this.txtFinalSizeY.Text = current_order.height.ToString();
+            this.txtQty.Text = current_order.quantity.ToString();
+            this.lstFinish.SelectedIndex = this.lstFinish.Items.IndexOf(lstFinish.Items.FindByText(current_order.stock_finish));
+            this.lstWeight.SelectedIndex = this.lstWeight.Items.IndexOf(lstWeight.Items.FindByText(current_order.stock_weight));
+            this.chkTwoSide.Checked = current_order.two_sided;
+            this.chkfolded.Checked = current_order.folded;
+            this.chkShip.Checked = current_order.ship;
+
         }
 
-        private string getCurrentOrderStatusDate(int orderID)
+        protected void func_Save(object sender, EventArgs e)
         {
             IOrderRepository orderRepository = RepositoryFactory.Get<IOrderRepository>();
-            IOrderHistoryRepository historyRepository = RepositoryFactory.Get<IOrderHistoryRepository>();
 
-            Order currentOrder = orderRepository.GetById(orderID);
+            if (Session["PWAS_order_id"] != null)
+            {
+                int orderID = System.Int32.Parse(Session["PWAS_order_id"].ToString());
+                Order orderEdit = orderRepository.GetById(orderID);
 
-            OrderHistory currentHistory = historyRepository.OrderHistories.Single(x => x.orderId == orderID && x.statusId == currentOrder.Status.statusId);// & x.statusId = currentOrder.Status);
+                if (validateFields())
+                {
+                    orderEdit.userID = getUserID();
+                    orderEdit.job_name = this.txtJobName.Text;
+                    orderEdit.width = Int32.Parse(this.txtFinalSizeX.Text);
+                    orderEdit.height = Int32.Parse(this.txtFinalSizeY.Text);
+                    orderEdit.quantity = Int32.Parse(this.txtQty.Text);
+                    orderEdit.stock_finish = this.lstFinish.SelectedValue;
+                    orderEdit.stock_weight = this.lstWeight.SelectedValue;
+                    orderEdit.two_sided = this.chkTwoSide.Checked;
+                    orderEdit.folded = this.chkfolded.Checked;
+                    orderEdit.ship = this.chkShip.Checked;
 
-            return currentHistory.modifiedDate.ToString();            
+                    orderRepository.SubmitChanges();
+
+                    this.lblNotify.Text = "Order Edited Sucessfull!  ";
+                    this.lblNotify.ForeColor = System.Drawing.Color.Blue;
+                    this.lblNotify.Visible = true;
+                    func_clearFields(false);
+
+                    Response.Redirect("customer_view_order.aspx");
+
+                }
+            }
+            else
+            {
+                Response.Redirect("customer_view_order.aspx");
+            }
+        }
+
+        private void func_View(object sender, CommandEventArgs e)
+        {
+            int orderID = System.Int32.Parse(e.CommandArgument.ToString());
+            Session["PWAS_order_id"] = orderID;
+            Response.Redirect("view_Single_Order.aspx");
+        }
+
+        protected void func_Clear(object sender, EventArgs e)
+        {
+            func_clearFields(true);
+        }
+        private string getOrderPrice(int orderID)
+        {
+            return "NOT IMPLE";
+        }
+        private void func_clearFields(bool notify)
+        {
+            if (notify)
+                this.lblNotify.Visible = false;
+            this.txtJobName.Text = "";
+            this.txtFinalSizeX.Text = "";
+            this.txtFinalSizeY.Text = "";
+            this.txtQty.Text = "";
+            this.chkTwoSide.Checked = false;
+            this.chkfolded.Checked = false;
+            this.chkShip.Checked = false;
+        }
+        private int getUserID()
+        {
+            return (int)Session[Constants.PWAS_SESSION_ID];
+        }
+        private bool validateFields()
+        {
+            int test;
+
+            bool ret = true;
+            if (this.txtJobName.Text == ""
+                || this.txtFinalSizeX.Text == ""
+                || this.txtFinalSizeY.Text == ""
+                || this.txtQty.Text == "")
+            {
+                this.lblNotify.Text = "You need to complete all the fields!";
+                this.lblNotify.Visible = true;
+                this.lblNotify.BorderColor = System.Drawing.Color.Red;
+                this.lblNotify.ForeColor = System.Drawing.Color.Red;
+                ret = false;
+            }
+            try
+            {
+                test = Int32.Parse(this.txtFinalSizeX.Text);
+                test = Int32.Parse(this.txtFinalSizeY.Text);
+                test = Int32.Parse(this.txtQty.Text);
+            }
+            catch (FormatException)
+            {
+                this.lblNotify.Text = "Bad Input in some field that are Integer required";
+                this.lblNotify.Visible = true;
+                this.lblNotify.BorderColor = System.Drawing.Color.Red;
+                this.lblNotify.ForeColor = System.Drawing.Color.Red;
+                ret = false;
+            }
+            return ret;
         }
 
     }
